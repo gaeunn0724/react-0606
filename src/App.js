954d21cd32566ae002ea32f43bc0672cda2d21cc
gaeunn0724 <gaeunn0724@gmail.com>
2022-06-20 04:21:21 +0900

    Read의 Route화

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import './App.css';
 import { useState } from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import { Link } from 'react-router-dom'; //component임
+import { Link, Routes, Route, useParams } from 'react-router-dom'; //component임
 import { HeaderStyled } from './HeaderStyled';
 import { Nav } from './Nav';
 import { Article } from './Article';
 import { Create } from './Create';
+
+function Read(props){
+  const params = useParams(); //가변적인 값(ex. id)를 props로 부터 가져올 수 있다.
+  const id = Number(params.id);
+  const topic = props.topics.filter(e=>{
+    if(e.id === id) //state의 id와 topics의 id가 같으면
+      return true;
+    else return false;
+  })[0];
+  return <Article title={topic.title} body={topic.body}></Article>
+}
+
 
 function App() {
   const [mode, setMode] = useState('WELCOME'); //새로운 상태의 기본 값이 welcome. let보다는 const
@@ -25,38 +37,50 @@ function App() {
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello, WEB!"></Article>
   }else if(mode === 'READ'){
-    const topic = topics.filter(e=>{
-      if(e.id === id) //state의 id와 topics의 id가 같으면
-        return true;
-      else return false;
-    })[0];
-    content = <Article title={topic.title} body={topic.body}></Article>
+    
   }else if(mode === 'CREATE'){
-
-    content = <Create onCreate={(title,body)=>{
-      const newTopic= {id:nextId, title:title, body:body};
-      // topics.push(newTopic);
-      // setTopics(topics);
-      const newTopics = [...topics];
-      newTopics.push(newTopic);
-      setTopics(newTopics);
-      setMode('READ');
-      setId(nextId);
-      setNextId(nextId+1);
-    }}/>;
   }
   return (
     <div>
       <HeaderStyled onSelect={headerHandler()}></HeaderStyled>
       <Nav data={topics} onSelect={navHandler()}></Nav>
-      {content}
+      {/* {content} */}
+      <Routes>
+        <Route path="/" element={<Article title="Welcome" body="Hello, WEB!"></Article>}></Route>
+        <Route path="/create" element={<Create onCreate={onCreateHandler()}/>}></Route>
+        <Route path="/read/:id" element={<Read topics={topics}/>}></Route>
+      </Routes>
       <ButtonGroup>
-        <Button variant="outlined" onClick={createHandler()}>Create</Button>
+      <Button component={Link} to="/create" variant="outlined" onClick={createHandler()}>Create</Button>
         <Button variant="outlined">Update</Button>
       </ButtonGroup>
-      <Button component={Link} to="/create" variant="outlined" onClick={deleteHandler()}>Delete</Button>
+      <Button component={Link} to="/delete" variant="outlined" onClick={deleteHandler()}>Delete</Button>
       </div>
   );
+  function Read(props){
+    const params = useParams();
+    const id = Number(params.id);
+    const topic = props.topics.filter(e=>{
+      if(e.id === id) {
+        return true;
+      } else {
+        return false;
+      }
+    })[0];
+    return <Article title={topic.title} body={topic.body}></Article>
+  }
+
+  function onCreateHandler() {
+    return (title, body) => {
+      const newTopic = { id: nextId, title: title, body: body };
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId + 1);
+    };
+  }
 
   function navHandler() {
     return (id) => {
